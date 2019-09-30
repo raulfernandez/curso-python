a5158f758ebdf00ee256da64a839cef3af0fdc46
Raul Fernandez <raul.fernandez@adidas.com>
2019-09-30 20:18:48 +0200

    update plataforma.proyecto and plataforma.empresa

--- Plataforma/Empresa.py
+++ Plataforma/Empresa.py
@@ -1,3 +1,52 @@
+class ProyectoNoEncontradoError(Exception):
+    pass
+
+
+class ObjectoEmpresaInvalidoError(Exception):
+    pass
+
+
 class Empresa:
-    def __init__(self, nombre: str):
+    def __init__(self, nombre: str, projectos: []):
         self.__nombre = nombre
+        self.__projectos = projectos
+
+    def agregar_proyecto(self, proyecto_id: str):
+        """
+        Agrega un proyecto al empresa.
+        :param proyecto_id: El identificador del proyecto
+        :return:
+        """
+        self.__proyectos.append(proyecto_id)
+
+    def empezar_proyecto(self, proyecto_id):
+        """
+        Empieza la ejecución de un projecto.
+        :param proyecto_id: El identificador del proyecto que se desea iniciar
+        :raise ProyectoNoEncontradoError En el caso que se intente empezar un projecto que no existe
+        :return:
+        """
+        if proyecto_id not in self.__projectos:
+            raise ProyectoNoEncontradoError(f"No se ha encontrado un projecto con el id {proyecto_id}")
+
+        pass
+
+    def serializar(self):
+        """
+        Serializa el objecto Empresa
+        i.e.: `microsoft;;adi1,contoso2`
+
+        :return: Una string con el objeto empresa serializado. Las propiedades separadas por `;` y los elementos de las
+        listas separadas por `,`.
+        """
+        # Serializar proyectos
+        proyectos = ','.join(self.__proyectos)
+        return f'{self.__nombre};{proyectos}'
+
+    @staticmethod
+    def desserializar(empresa_str: str):
+        props = empresa_str.split(';')
+        if len(props) == 6:
+            return Empresa(props[0], props[1].split(','))
+        else:
+            raise ObjectoEmpresaInvalidoError
--- Plataforma/Proyecto.py
+++ Plataforma/Proyecto.py
@@ -1,9 +1,84 @@
 from Plataforma import Usuario
+from Plataforma.Utils import Serializador
 
 
-class Proyecto:
-    def agregar_usuario(self, usuario: Usuario):
-        pass
+class LimiteDeUsuariosError(Exception):
+    pass
+
+
+class ObjectoProyectoInvalidoError(Exception):
+    pass
+
+
+MAX_NUMERO_USUARIOS = 3
+
+
+class Proyecto(Serializador):
+    def __init__(self, empresa, titulo: str, id: str, categoria="", candidatos=[], asignado=None, estado=False,
+                 max_numero_candidatos=MAX_NUMERO_USUARIOS):
+        self.__empresa = empresa
+        self.__titulo = titulo
+        self.__id = id
+        self.__usuario_asignado = asignado
+        self.__proyecto_finalizado = estado
+        self.__categoria = categoria
+        self.__candidatos = candidatos
+        self.__max_numero_candidatos = max_numero_candidatos
+
+    @property
+    def empresa(self):
+        return self.__empresa
+
+    @property
+    def categoria(self):
+        return self.__categoria
+
+    @property
+    def identificador(self):
+        return self.__id
+
+    @property
+    def titulo(self):
+        return self.__titulo
+
+    @property
+    def asignado(self):
+        return self.__usuario_asignado
+
+    @property
+    def finalizado(self):
+        return self.__proyecto_finalizado
+
+    @property
+    def disponible(self):
+        return self.__usuario_asignado is None and len(self.__usuarios) < self.__max_numero_candidatos
+
+    def agregar_candidato(self, candidato: Usuario):
+        if not self.__es_equipo_lleno():
+            self.__candidatos.append(candidato)
+        else:
+            raise LimiteDeUsuariosError("No se pueden añadir más candidatos al proyecto")
+
+    def elegir_candidato(self):
+        if len(self.__usuarios):
+            return False
+        else:
+            self.__mi_algoritmo_de_asignacion()
+
+    def finalizar_proyecto(self):
+        self.__proyecto_finalizado = True
+
+    def __mi_algoritmo_de_asignacion(self):
+        elegido = None
+        for candidato in self.__candidatos:
+            if elegido is None or candidato.puntuacion > elegido.puntuacion:
+                elegido = candidato
+        self.__usuario_asignado = elegido
 
     def __es_equipo_lleno(self) -> bool:
-        pass
+        return len(self.__usuarios) > self.__max_numero_candidatos
+
+    @classmethod
+    def deserializar(cls, serialized_str: str):
+        init_str = super().deserializar(serialized_str=serialized_str)
+        return eval(init_str)
