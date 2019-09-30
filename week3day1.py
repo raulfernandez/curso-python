a4c4a17eb7d888c7c9bfd0e7096d673cc0a09d27
Raul Fernandez <raul.fernandez@adidas.com>
2019-09-30 20:54:28 +0200

    usage of json load and dump

--- week3day1.py
+++ week3day1.py
@@ -1,19 +1,42 @@
 from Plataforma.Usuario import Usuario
-from typing import List
+from json import load, dump
+import collections
+
+def load_json(file_name):
+    obj = {}
+
+    try:
+        json_file = open(f'C:/git/curso-python/json/{file_name}', 'r')
+        obj = load(json_file)
+        json_file.close()
+    except Exception as ex:
+        print('Error', ex)
+
+    return obj
 
 
-def procesar():
-    return iter([par for par in range(100) if par % 2 == 0])
+def dump_json(file_name, obj):
+    try:
+        json_file = open(f'C:/git/curso-python/json/{file_name}', 'w')
+        dump(obj, json_file)
+        json_file.close()
+    except Exception as ex:
+        print('Error', ex)
 
 
-class Coche:
-    pass
+# URL = 'https://jsonplaceholder.typicode.com/'
 
 
 def main():
-    # gen = procesar()
-    # print(next(gen))
-    # print(next(gen))
-    # print(next(filter(lambda x: x > 20, [par for par in range(100) if par % 2 == 0])))
-    usr = Usuario('jsmith', 'John', 'Smith')
-    print(usr.serializar())
+    users = load_json('usuarios.json')
+    posts = load_json('posts.json')
+
+    for user in users:
+        user['posts'] = \
+            [post for post in posts if post['userId'] == user['id']][0:2]
+
+    dump_json('usuarios_con_post.json', users)
+
+
+if __name__ == '__main__':
+    main()
